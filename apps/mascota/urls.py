584e4ee54a152419b676f1bdca7d040b59ab2e28
daniel <daniel.cutipa@hotmail.com>
2019-01-26 13:38:25 -0400

    second commit

---
+++ apps/mascota/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from apps.mascota.views import index, mascota_view, mascota_list
+
+urlpatterns = [
+    path('', index, name='index'),
+    path('nuevo', mascota_view, name='mascota_crear'),
+    path('listar', mascota_list, name='mascota_listar'),
+]
